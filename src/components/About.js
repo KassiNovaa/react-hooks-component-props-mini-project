82b2cdf0ae30a9022bea2eed080e4398931373e6
Nicole Casteel <thepronicole888@gmail.com>
2023-10-03 10:12:58 -0500

    components

---
+++ src/components/About.js
@@ -0,0 +1,6 @@
+function (){
+    <aside>
+        <img className="image" src="https://via.placeholder.com/215" alt="Blog Logo"></img>
+        
+    </aside>
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+function BlogName({name}){
+    return (
+        <header> 
+            <h1>{blogData.name}</h1>
+        </header>
+    )
+};
+export default BlogName;
+ 
